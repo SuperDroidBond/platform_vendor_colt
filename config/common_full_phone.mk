DirtyUnicorns/android_vendor_du

World phone support
Change-Id: Ibfc2ba0d58dd55f01d43435817e0cedf6f05d6bd

config/common_full_phone.mk
@@ -1,6 +1,14 @@
 # Inherit common stuff
 $(call inherit-product, vendor/du/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml

SuperDroidBond/platform_vendor_colt

Signed-off-by: rdbsoft <raakesh.batra@rediffmail.com>

config/common_full_phone.mk
@@ -1,6 +1,14 @@
 # Inherit common stuff
 $(call inherit-product, vendor/colt/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/colt/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/colt/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
     vendor/colt/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
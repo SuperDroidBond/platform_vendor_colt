AOSiP/platform_vendor_aosip

vendor/aosip: Add LatinIME dictionaries support
Thanks to CyanogenMod

Change-Id: If528a90d316e5cebfb2c68b447678471b70427e4
Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

config/common.mk
@@ -50,6 +50,9 @@ PRODUCT_COPY_FILES +=  \
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosip/overlay/common
 
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosip/overlay/dictionaries
+
 # Packages
 include vendor/aosip/config/packages.mk
 

SuperDroidBond/platform_vendor_colt

Signed-off-by: rdbsoft <raakesh.batra@rediffmail.com>

config/common.mk
@@ -59,6 +59,9 @@ EXCLUDE_SYSTEMUI_TESTS := true
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/colt/overlay/common
 
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/colt/overlay/dictionaries
+
 # Packages
 include vendor/colt/config/packages.mk
 